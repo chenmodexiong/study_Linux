7cbe6e046379326d0ce6769cbc9417c7c580e41f
gitee_com_zb <18749379836@163.com>
2024-03-07 14:17:28 +0800

    基于信号的异步非阻塞式等待

--- 4.signal_信号捕捉/mysignal.cc
+++ 4.signal_信号捕捉/mysignal.cc
@@ -1,31 +1,51 @@
 #include <iostream>
 #include <signal.h>
 #include <string.h>
+#include <ctime>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 using namespace std;
 
 void handler(int signo)
 {
-    cout<<"I am process,pid:"<<getpid()<<" catch a signal,signo:"<<signo<<endl;
+    sleep(2);
+    pid_t rid;
+    // 基于信号的异步非阻塞式等待
+    while ((rid = waitpid(-1, nullptr, WNOHANG)) > 0)
+    {
+        cout << "I am process,pid:" << getpid() << " catch a signal,signo:" << signo;
+        cout << " child quit,pid:" << rid << endl;
+    }
 }
 int main()
 {
-    //SIGCHLD(17)
-    signal(SIGCHLD,handler);
-    pid_t id=fork();
-    if(id==0)
+    /* 直接将17号信号的处理设为忽略方式，不需要再手动调用wait进行回收*/
+    /* 注意!!!此方式在Linux系统中有效，其他类UNIX系统并不保证*/
+    // signal(SIGCHLD,SIG_IGN);
+
+    signal(SIGCHLD, handler);
+    for (int i = 0; i < 10; i++)
     {
-        while(true)
+        pid_t id = fork();
+        if (id == 0)
         {
-            cout<<"I am child process,pid:"<<getpid()<<" ppid:"<<getppid()<<endl;
-            sleep(1);
-            break;
+            while (true)
+            {
+                cout << "I am child process,pid:" << getpid() << " ppid:" << getppid() << endl;
+                sleep(1);
+                break;
+            }
+            cout << "child quit" << endl;
+            exit(0);
         }
-        exit(0);
+        sleep(rand() % 5 + 3);
     }
-    while(true)
+
+    while (true)
     {
-        cout<<"I am father process,pid:"<<getpid()<<endl;
+        cout << "I am father process,pid:" << getpid() << endl;
         sleep(1);
     }
 
@@ -33,17 +53,18 @@ int main()
 }
 
 #ifdef __TEST_CODE__
-volatile int flag=0;//volatile防止编译器进行过度优化，保持内存可见性
+volatile int flag = 0; // volatile防止编译器进行过度优化，保持内存可见性
 void handler(int signo)
 {
-    cout<<"catch a signal:"<<signo<<endl;
-    flag=1;
+    cout << "catch a signal:" << signo << endl;
+    flag = 1;
 }
 int main()
 {
-    signal(2,handler);
-    while(!flag); //编译器可能对其进行一定程度的优化
-    cout<<"process quit normal"<<endl;
+    signal(2, handler);
+    while (!flag)
+        ; // 编译器可能对其进行一定程度的优化
+    cout << "process quit normal" << endl;
     return 0;
 }
 #endif
@@ -80,9 +101,9 @@ int main()
     memset(&oact, 0, sizeof(oact));
     act.sa_handler = handler; // SIG_DEF --默认  SIG_IGN --忽略
     sigemptyset(&act.sa_mask);
-    sigaddset(&act.sa_mask,1);
-    sigaddset(&act.sa_mask,3);
-    sigaddset(&act.sa_mask,4);
+    sigaddset(&act.sa_mask, 1);
+    sigaddset(&act.sa_mask, 3);
+    sigaddset(&act.sa_mask, 4);
     sigaction(2, &act, &oact);
     while (true)
     {
